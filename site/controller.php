610bf0e2f9f571fb3040065f81b77944215d6b2b
Qawelesizwe <qawemlilo@gmail.com>
2011-10-24 13:16:45 +0200

    Created directory structure

---
+++ site/controller.php
@@ -0,0 +1,25 @@
+<?php
+// No direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+
+/**
+ * Hello World Component Controller
+ *
+ * @package    Joomla.Tutorials
+ * @subpackage Components
+ */
+class UploadsController extends JController
+{
+	/**
+	 * Method to display the view
+	 *
+	 * @access    public
+	 */
+	function display()
+	{
+		parent::display();
+	}
+
+}
